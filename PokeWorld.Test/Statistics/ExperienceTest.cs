798a4b00569d1ae25d0b677f5e4b2adb6ca691a4
Tana <finaltruth.shintai@gmail.com>
2020-02-21 01:26:37 +0900

    経験値を実装。

---
+++ PokeWorld.Test/Statistics/ExperienceTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PokeWorld.Statistics;
+
+namespace PokeWorld.Test.Statistics
+{
+    [TestFixture]
+    public class ExperienceTest
+    {
+        [Test]
+        public void TestNew()
+        {
+            Assert.That(() => new Experience(-1), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+            Assert.That(() => new Experience(0), Throws.Nothing);
+            Assert.That(() => new Experience(int.MaxValue), Throws.Nothing);
+        }
+
+        [Test]
+        public void TestValue()
+        {
+            var exp1 = new Experience(197);
+            Assert.That(exp1.Value, Is.EqualTo(197));
+
+            var exp2 = new Experience(1);
+            Assert.That(exp2.Value, Is.EqualTo(1));
+        }
+        
+        [Test]
+        public void TestToString()
+        {
+            var exp1 = new Experience(197);
+            Assert.That(exp1.ToString(), Is.EqualTo("197"));
+
+            var exp2 = new Experience(1);
+            Assert.That(exp2.ToString(), Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void TestEquals()
+        {
+            var exp1 = new Experience(197);
+            Assert.That(exp1.ToString(), Is.EqualTo("197"));
+
+            var exp2 = new Experience(1);
+            Assert.That(exp2.ToString(), Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void TestSubtract()
+        {
+            var exp1 = new Experience(1);
+            var exp2 = new Experience(2);
+            Assert.That(exp2.Subtract(exp1).Value, Is.EqualTo(1));
+            // 引き算を行っても元のインスタンスの値は不変であることを確認する
+            Assert.That(exp2.Value, Is.EqualTo(2));
+
+            Assert.That(exp1.Subtract(exp1).Value, Is.EqualTo(0));
+            Assert.That(() => exp1.Subtract(exp2), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+
+        }
+
+        [Test]
+        public void TestZero()
+        {
+            Assert.That(Experience.Zero.Value, Is.EqualTo(0));
+        }
+    }
+}
---
+++ PokeWorld/Statistics/Experience.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PokeWorld.Statistics
+{
+    /// <summary>
+    /// 経験値を表します。
+    /// </summary>
+    public class Experience
+    {
+        /// <summary>経験値の最小値</summary>
+        private const int MinValue = 0;
+        /// <summary>経験値の最小値</summary>
+        private const int MaxValue = int.MaxValue;
+
+        /// <summary>経験値ゼロを表すインスタンス</summary>
+        public static readonly Experience Zero = new Experience(0);
+
+        /// <summary>
+        /// 経験値を取得します。
+        /// </summary>
+        public int Value { get; private set; }
+
+        /// <summary>
+        /// <see cref="Experience"/> の新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="value">経験値。</param>
+        public Experience(int value)
+        {
+            if (value < MinValue || value > MaxValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), value,
+                    $"経験値の範囲は {MinValue} ～ {MaxValue} です。");
+            }
+            Value = value;
+        }
+
+        /// <summary>
+        /// 現在の経験値から指定した経験値を引いた差を計算します。
+        /// </summary>
+        /// <param name="exp">引く経験値。</param>
+        /// <returns>現在の経験値から指定した経験値を引いた差。</returns>
+        public Experience Subtract(Experience exp)
+        {
+            if (Value - exp.Value < MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(exp), exp,
+                    $"差を計算すると経験値が負の値になります。");
+            }
+            return new Experience(Value - exp.Value);
+        }
+
+        /// <summary>
+        /// 経験値をそれと等価な文字列の値に変換します。
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => Value.ToString();
+    }
+}
