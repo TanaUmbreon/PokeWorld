42727aaf2ebb074a2ae7d43a06a5a4474ad5aed8
Tana <finaltruth.shintai@gmail.com>
2020-02-21 01:31:38 +0900

    ステータス決定におけるレベルと経験値周りの仕組みを実装。単体テスト未作成。

---
+++ PokeWorld/Statistics/Species.cs
@@ -0,0 +1,37 @@
+﻿using PokeWorld.DomainSupport;
+
+namespace PokeWorld.Statistics
+{
+    /// <summary>
+    /// ポケモンの種族を表します。
+    /// </summary>
+    public class Species : Enumeration
+    {
+        /// <summary>フシギダネ</summary>
+        public static readonly Species Bulbasaur = new Species(
+            id: 1,
+            name: "フシギダネ",
+            expType: ExperienceType.MediumSlow);
+
+        #region 列挙型クラスのインスタンス メンバ
+
+        /// <summary>
+        /// 経験値タイプを取得します。
+        /// </summary>
+        public ExperienceType ExpType { get; private set; }
+
+        /// <summary>
+        /// <see cref="Species"/> の新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="id">ポケモンの種族を一意に特定する為の識別子。</param>
+        /// <param name="name">種族名。</param>
+        /// <param name="expType">経験値タイプ。</param>
+        private Species(int id, string name, ExperienceType expType) :
+            base(id, name)
+        {
+            ExpType = expType;
+        }
+
+        #endregion
+    }
+}
--- PokeWorld/Statistics/Statistic.cs
+++ PokeWorld/Statistics/Statistic.cs
@@ -9,19 +9,35 @@ namespace PokeWorld.Statistics
     /// </summary>
     public class Statistic
     {
+        /// <summary>種族情報</summary>
+        private readonly Species species;
+        /// <summary>レベル & 経験値操作オブジェクト</summary>
+        private readonly LevelExperienceControler levelControler;
+
         /// <summary>
-        /// レベルの値を取得します。
+        /// レベルを取得します。
         /// </summary>
-        public Level Level { get; private set; }
+        public Level Level => levelControler.Level;
+
+        /// <summary>
+        /// 累計経験値を取得します。
+        /// </summary>
+        public Experience TotalExp => levelControler.TotalExp;
+
+        /// <summary>
+        /// 次のレベルまでの必要経験値を取得します。
+        /// </summary>
+        public Experience RequiredExpToNextLevel => levelControler.RequiredExpToNextLevel;
 
         /// <summary>
         /// <see cref="Statistic"/> の新しいインスタンスを生成します。
         /// </summary>
+        /// <param name="species">種族。</param>
         /// <param name="level">レベル。</param>
-        public Statistic(Level level)
+        public Statistic(Species species, Level level)
         {
-            Level = level;
+            this.species = species;
+            levelControler = new LevelExperienceControler(species.ExpType, level);
         }
-
     }
 }
