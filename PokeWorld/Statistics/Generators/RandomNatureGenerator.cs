47e11784b8e262e10807f890602ac3b1bd1a3fff
Tana <finaltruth.shintai@gmail.com>
2020-02-24 17:49:55 +0900

    せいかくを決定する仕組みを実装。

---
+++ PokeWorld/Statistics/Generators/RandomNatureGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using PokeWorld.DomainSupport;
+
+namespace PokeWorld.Statistics.Generators
+{
+    /// <summary>
+    /// 全せいかくから均一な確率でランダムにせいかくを生成するアルゴリズムです。
+    /// </summary>
+    public sealed class RandomNatureGenerator : INatureGenerator
+    {
+        #region 単一インスタンス
+
+        /// <summary>このクラスの単一のインスタンス</summary>
+        private static RandomNatureGenerator? instance;
+
+        /// <summary>
+        /// このクラスの単一のインスタンスを取得します。
+        /// </summary>
+        public static RandomNatureGenerator Instance => instance ??= new RandomNatureGenerator();
+
+        /// <summary>
+        /// <see cref="RandomNatureGenerator"/> の新しいインスタンスを生成します。
+        /// </summary>
+        private RandomNatureGenerator() { }
+
+        #endregion
+
+        /// <summary>全せいかくの配列</summary>
+        private readonly Nature[] natures = Enumeration.GetAll<Nature>().ToArray();
+        /// <summary>乱数生成オブジェクト</summary>
+        private readonly Random random = new Random();
+
+        /// <summary>
+        /// 全せいかくから均一な確率でランダムに生成し返します。
+        /// </summary>
+        /// <returns>せいかく。</returns>
+        public Nature Generate() => natures[random.Next(natures.Length)];
+    }
+}
---
+++ PokeWorld/Statistics/INatureGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace PokeWorld.Statistics
+{
+    /// <summary>
+    /// せいかくを生成する為のアルゴリズムです。
+    /// </summary>
+    public interface INatureGenerator
+    {
+        /// <summary>
+        /// せいかくをアルゴリズムに従って生成し返します。
+        /// </summary>
+        /// <returns>せいかく。</returns>
+        Nature Generate();
+    }
+}
