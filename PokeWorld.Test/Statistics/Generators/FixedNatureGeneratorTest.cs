55ad69115fd4d0e08fa22aeb0148c261ebe7df94
Tana <finaltruth.shintai@gmail.com>
2020-02-24 18:01:13 +0900

    せいかく生成アルゴリズムを追加。

---
+++ PokeWorld.Test/Statistics/Generators/FixedNatureGeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PokeWorld.Statistics;
+using PokeWorld.Statistics.Generators;
+
+namespace PokeWorld.Test.Statistics.Generators
+{
+    [TestFixture]
+    public class FixedNatureGeneratorTest
+    {
+        [Test]
+        public void TestNew()
+        {
+            Assert.That(() => new FixedNatureGenerator(Nature.Hardy), Throws.Nothing);
+        }
+
+        [Test]
+        public void TestGenerate()
+        {
+            var gen = new FixedNatureGenerator(Nature.Hardy);
+            Assert.That(gen.Generate(), Is.EqualTo(Nature.Hardy));
+        }
+    }
+}
---
+++ PokeWorld/Statistics/Generators/FixedNatureGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace PokeWorld.Statistics.Generators
+{
+    /// <summary>
+    /// 任意に固定されたせいかくを生成するアルゴリズムです。
+    /// </summary>
+    public class FixedNatureGenerator : INatureGenerator
+    {
+        /// <summary>生成するせいかく</summary>
+        private readonly Nature nature;
+
+        /// <summary>
+        /// <see cref="FixedNatureGenerator"/> の新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="nature">生成するせいかく。</param>
+        public FixedNatureGenerator(Nature nature) => this.nature = nature;
+
+        /// <summary>
+        /// 任意に固定されたせいかくを生成し返します。
+        /// </summary>
+        /// <returns>せいかく。</returns>
+        public Nature Generate() => nature;
+    }
+}
