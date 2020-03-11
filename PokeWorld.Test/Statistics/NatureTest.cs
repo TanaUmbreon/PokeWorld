9f08bc25143cabf06997b5e9c57d2f64a329299d
Tana <finaltruth.shintai@gmail.com>
2020-03-12 02:55:18 +0900

    せいかくの全パターンを実装。

--- PokeWorld.Test/Statistics/NatureTest.cs
+++ PokeWorld.Test/Statistics/NatureTest.cs
@@ -19,6 +19,246 @@ namespace PokeWorld.Test.Statistics
                 Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
                 Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
             }
+            {
+                Nature member = Nature.Lonely;
+                Assert.That(member.Id, Is.EqualTo(1));
+                Assert.That(member.Name, Is.EqualTo("さみしがり"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Brave;
+                Assert.That(member.Id, Is.EqualTo(2));
+                Assert.That(member.Name, Is.EqualTo("ゆうかん"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.Decreased));
+            }
+            {
+                Nature member = Nature.Adamant;
+                Assert.That(member.Id, Is.EqualTo(3));
+                Assert.That(member.Name, Is.EqualTo("いじっぱり"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Naughty;
+                Assert.That(member.Id, Is.EqualTo(4));
+                Assert.That(member.Name, Is.EqualTo("やんちゃ"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Bold;
+                Assert.That(member.Id, Is.EqualTo(5));
+                Assert.That(member.Name, Is.EqualTo("ずぶとい"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Docile;
+                Assert.That(member.Id, Is.EqualTo(6));
+                Assert.That(member.Name, Is.EqualTo("すなお"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Relaxed;
+                Assert.That(member.Id, Is.EqualTo(7));
+                Assert.That(member.Name, Is.EqualTo("のんき"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.Decreased));
+            }
+            {
+                Nature member = Nature.Impish;
+                Assert.That(member.Id, Is.EqualTo(8));
+                Assert.That(member.Name, Is.EqualTo("わんぱく"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Lax;
+                Assert.That(member.Id, Is.EqualTo(9));
+                Assert.That(member.Name, Is.EqualTo("のうてんき"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Timid;
+                Assert.That(member.Id, Is.EqualTo(10));
+                Assert.That(member.Name, Is.EqualTo("おくびょう"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.Increased));
+            }
+            {
+                Nature member = Nature.Hasty;
+                Assert.That(member.Id, Is.EqualTo(11));
+                Assert.That(member.Name, Is.EqualTo("せっかち"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.Increased));
+            }
+            {
+                Nature member = Nature.Serious;
+                Assert.That(member.Id, Is.EqualTo(12));
+                Assert.That(member.Name, Is.EqualTo("まじめ"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Jolly;
+                Assert.That(member.Id, Is.EqualTo(13));
+                Assert.That(member.Name, Is.EqualTo("ようき"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.Increased));
+            }
+            {
+                Nature member = Nature.Naive;
+                Assert.That(member.Id, Is.EqualTo(14));
+                Assert.That(member.Name, Is.EqualTo("むじゃき"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.Increased));
+            }
+            {
+                Nature member = Nature.Modest;
+                Assert.That(member.Id, Is.EqualTo(15));
+                Assert.That(member.Name, Is.EqualTo("ひかえめ"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Mild;
+                Assert.That(member.Id, Is.EqualTo(16));
+                Assert.That(member.Name, Is.EqualTo("おっとり"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Quiet;
+                Assert.That(member.Id, Is.EqualTo(17));
+                Assert.That(member.Name, Is.EqualTo("れいせい"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.Decreased));
+            }
+            {
+                Nature member = Nature.Bashful;
+                Assert.That(member.Id, Is.EqualTo(18));
+                Assert.That(member.Name, Is.EqualTo("てれや"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Rash;
+                Assert.That(member.Id, Is.EqualTo(19));
+                Assert.That(member.Name, Is.EqualTo("うっかりや"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Calm;
+                Assert.That(member.Id, Is.EqualTo(20));
+                Assert.That(member.Name, Is.EqualTo("おだやか"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Gentle;
+                Assert.That(member.Id, Is.EqualTo(21));
+                Assert.That(member.Name, Is.EqualTo("おとなしい"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Sassy;
+                Assert.That(member.Id, Is.EqualTo(22));
+                Assert.That(member.Name, Is.EqualTo("なまいき"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.Decreased));
+            }
+            {
+                Nature member = Nature.Careful;
+                Assert.That(member.Id, Is.EqualTo(23));
+                Assert.That(member.Name, Is.EqualTo("しんちょう"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.Decreased));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.Increased));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
+            {
+                Nature member = Nature.Quirky;
+                Assert.That(member.Id, Is.EqualTo(24));
+                Assert.That(member.Name, Is.EqualTo("きまぐれ"));
+                Assert.That(member.Attack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Defense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpAttack, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.SpDefense, Is.EqualTo(NatureCorrection.NoChange));
+                Assert.That(member.Speed, Is.EqualTo(NatureCorrection.NoChange));
+            }
         }
     }
 }
--- PokeWorld/Statistics/Nature.cs
+++ PokeWorld/Statistics/Nature.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using PokeWorld.DomainSupport;
+﻿using PokeWorld.DomainSupport;
 
 namespace PokeWorld.Statistics
 {
@@ -14,6 +11,270 @@ namespace PokeWorld.Statistics
         public static readonly Nature Hardy = new Nature(
             id: 0,
             name: "がんばりや",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>さみしがり</summary>
+        public static readonly Nature Lonely = new Nature(
+            id: 1,
+            name: "さみしがり",
+            attack: NatureCorrection.Increased,
+            defense: NatureCorrection.Decreased,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>ゆうかん</summary>
+        public static readonly Nature Brave = new Nature(
+            id: 2,
+            name: "ゆうかん",
+            attack: NatureCorrection.Increased,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.Decreased
+            );
+
+        /// <summary>いじっぱり</summary>
+        public static readonly Nature Adamant = new Nature(
+            id: 3,
+            name: "いじっぱり",
+            attack: NatureCorrection.Increased,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.Decreased,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>やんちゃ</summary>
+        public static readonly Nature Naughty = new Nature(
+            id: 4,
+            name: "やんちゃ",
+            attack: NatureCorrection.Increased,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.Decreased,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>ずぶとい</summary>
+        public static readonly Nature Bold = new Nature(
+            id: 5,
+            name: "ずぶとい",
+            attack: NatureCorrection.Decreased,
+            defense: NatureCorrection.Increased,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>すなお</summary>
+        public static readonly Nature Docile = new Nature(
+            id: 6,
+            name: "すなお",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>のんき</summary>
+        public static readonly Nature Relaxed = new Nature(
+            id: 7,
+            name: "のんき",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.Increased,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.Decreased
+            );
+
+        /// <summary>わんぱく</summary>
+        public static readonly Nature Impish = new Nature(
+            id: 8,
+            name: "わんぱく",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.Increased,
+            spAttack: NatureCorrection.Decreased,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>のうてんき</summary>
+        public static readonly Nature Lax = new Nature(
+            id: 9,
+            name: "のうてんき",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.Increased,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.Decreased,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>おくびょう</summary>
+        public static readonly Nature Timid = new Nature(
+            id: 10,
+            name: "おくびょう",
+            attack: NatureCorrection.Decreased,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.Increased
+            );
+
+        /// <summary>せっかち</summary>
+        public static readonly Nature Hasty = new Nature(
+            id: 11,
+            name: "せっかち",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.Decreased,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.Increased
+            );
+
+        /// <summary>まじめ</summary>
+        public static readonly Nature Serious = new Nature(
+            id: 12,
+            name: "まじめ",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>ようき</summary>
+        public static readonly Nature Jolly = new Nature(
+            id: 13,
+            name: "ようき",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.Decreased,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.Increased
+            );
+
+        /// <summary>むじゃき</summary>
+        public static readonly Nature Naive = new Nature(
+            id: 14,
+            name: "むじゃき",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.Decreased,
+            speed: NatureCorrection.Increased
+            );
+
+        /// <summary>ひかえめ</summary>
+        public static readonly Nature Modest = new Nature(
+            id: 15,
+            name: "ひかえめ",
+            attack: NatureCorrection.Decreased,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.Increased,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>おっとり</summary>
+        public static readonly Nature Mild = new Nature(
+            id: 16,
+            name: "おっとり",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.Decreased,
+            spAttack: NatureCorrection.Increased,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>れいせい</summary>
+        public static readonly Nature Quiet = new Nature(
+            id: 17,
+            name: "れいせい",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.Increased,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.Decreased
+            );
+
+        /// <summary>てれや</summary>
+        public static readonly Nature Bashful = new Nature(
+            id: 18,
+            name: "てれや",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.NoChange,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>うっかりや</summary>
+        public static readonly Nature Rash = new Nature(
+            id: 19,
+            name: "うっかりや",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.Increased,
+            spDefense: NatureCorrection.Decreased,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>おだやか</summary>
+        public static readonly Nature Calm = new Nature(
+            id: 20,
+            name: "おだやか",
+            attack: NatureCorrection.Decreased,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.Increased,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>おとなしい</summary>
+        public static readonly Nature Gentle = new Nature(
+            id: 21,
+            name: "おとなしい",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.Decreased,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.Increased,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>なまいき</summary>
+        public static readonly Nature Sassy = new Nature(
+            id: 22,
+            name: "なまいき",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.NoChange,
+            spDefense: NatureCorrection.Increased,
+            speed: NatureCorrection.Decreased
+            );
+
+        /// <summary>しんちょう</summary>
+        public static readonly Nature Careful = new Nature(
+            id: 23,
+            name: "しんちょう",
+            attack: NatureCorrection.NoChange,
+            defense: NatureCorrection.NoChange,
+            spAttack: NatureCorrection.Decreased,
+            spDefense: NatureCorrection.Increased,
+            speed: NatureCorrection.NoChange
+            );
+
+        /// <summary>きまぐれ</summary>
+        public static readonly Nature Quirky = new Nature(
+            id: 24,
+            name: "きまぐれ",
             attack: NatureCorrection.NoChange,
             defense: NatureCorrection.NoChange,
             spAttack: NatureCorrection.NoChange,
