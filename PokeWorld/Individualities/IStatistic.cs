adbc325ffb95c7e968647127b519ccca289a66f8
Tana <finaltruth.shintai@gmail.com>
2020-12-17 21:21:05 +0900

    XMLコメントを追加。

--- PokeWorld/Individualities/IStatistic.cs
+++ PokeWorld/Individualities/IStatistic.cs
@@ -1,20 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PokeWorld.Individualities
+﻿namespace PokeWorld.Individualities
 {
+    /// <summary>
+    /// 能力値を実装します。
+    /// </summary>
     public interface IStatistic { }
 
+    /// <summary>
+    /// HP を表します。
+    /// </summary>
     public class HitPoints : IStatistic { }
 
+    /// <summary>
+    /// こうげきを表します。
+    /// </summary>
     public class Attack : IStatistic { }
 
+    /// <summary>
+    /// ぼうぎょを表します。
+    /// </summary>
     public class Defense : IStatistic { }
 
+    /// <summary>
+    /// とくこうを表します。
+    /// </summary>
     public class SpecialAttack : IStatistic { }
 
+    /// <summary>
+    /// とくぼうを表します。
+    /// </summary>
     public class SpecialDefense : IStatistic { }
 
+    /// <summary>
+    /// すばやさを表します。
+    /// </summary>
     public class Speed : IStatistic { }
 }
