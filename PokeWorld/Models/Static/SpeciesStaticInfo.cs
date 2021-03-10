834e909923352ba3db1c04f9f800db1bd461a509
Tana <finaltruth.shintai@gmail.com>
2021-03-10 21:12:41 +0900

    バトルでのポケモンを表すクラスとテストコードを追加。

---
+++ PokeWorld/Models/Static/SpeciesStaticInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokeWorld.Models.Static
+{
+    /// <summary>
+    /// 種族の静的データを格納します。
+    /// </summary>
+    public class SpeciesStaticInfo
+    {
+        /// <summary>
+        /// インスタンスを一意に特定する識別子を取得します。
+        /// </summary>
+        public string Key { get; init; }
+
+        /// <summary>
+        /// 図鑑番号の静的データを取得します。
+        /// </summary>
+        public PokedexNumberStaticInfo PokedexNumber { get; init; }
+
+        /// <summary>
+        /// <see cref="SpeciesStaticInfo"/> の新しいインスタンスを生成します。
+        /// </summary>
+        public SpeciesStaticInfo(string key, PokedexNumberStaticInfo pokedexNumber)
+        {
+            Key = key;
+            PokedexNumber = pokedexNumber;
+        }
+
+        #region データ参照をサポートする為のメソッド
+
+        /// <summary>
+        /// 全国図鑑番号を取得します。
+        /// </summary>
+        public int GetNationalPokedexNumber() => PokedexNumber.National;
+
+        #endregion
+    }
+}
