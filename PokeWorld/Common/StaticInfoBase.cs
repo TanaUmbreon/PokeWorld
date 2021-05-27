770bd28bced07dd5b6b750e120d6aa96b81c70df
Tana <finaltruth.shintai@gmail.com>
2021-05-28 02:00:03 +0900

    静的データの仕様を見直し。

---
+++ PokeWorld/Common/StaticInfoBase.cs
@@ -0,0 +1,13 @@
+﻿namespace PokeWorld.Common
+{
+    /// <summary>
+    /// 静的データを格納する基底クラスです。このクラスは不変です。
+    /// </summary>
+    public abstract record StaticInfoBase(string Key)
+    {
+        /// <summary>
+        /// インスタンスを一意に特定する文字列型識別子を取得します。
+        /// </summary>
+        public string Key { get; init; } = Key;
+    }
+}
