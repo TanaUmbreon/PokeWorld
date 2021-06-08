c89331cd87b537fc7de2ae24949aba3f94503185
Tana <finaltruth.shintai@gmail.com>
2021-06-08 23:38:13 +0900

    静的データの操作を実装。

---
+++ PokeWorld/Infrastracures/JsonStaticRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using PokeWorld.Common;
+using PokeWorld.Models;
+using PokeWorld.Repositories;
+
+namespace PokeWorld.Infrastracures
+{
+    /// <summary>
+    /// JSON 形式で定義された静的データ ファイルへの物理的な操作を実装します。
+    /// </summary>
+    public class JsonStaticRepository : IStaticRepository
+    {
+        public KeyRecordCollection<Species> Species { get; }
+
+        /// <summary>
+        /// <see cref="JsonStaticRepository"/> の新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="staticRootDirectoryPath">JSON ファイル形式の静的データが格納されたルート フォルダーのパス。</param>
+        public JsonStaticRepository(string staticRootDirectoryPath)
+        {
+            var directory = new DirectoryInfo(staticRootDirectoryPath);
+            if (!directory.Exists)
+            {
+                throw new DirectoryNotFoundException($"静的データのルート フォルダー '{directory.FullName}' は存在しません。");
+            }
+
+            Species = LoadAsKeyRecordCollection<Species>(Path.Combine(directory.FullName, "Natures.json"));
+        }
+
+        /// <summary>
+        /// パス指定した JSON 形式の静的データ ファイルを読み込み、 <see cref="KeyRecordCollection{T}"/> に変換して返します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filePath">読み込む JSON 形式の静的データ ファイルのパス。</param>
+        /// <param name="converters"></param>
+        /// <returns></returns>
+        private static KeyRecordCollection<T> LoadAsKeyRecordCollection<T>(string filePath, params JsonConverter[] converters) where T : KeyRecord
+        {
+            var file = new FileInfo(filePath);
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException($"静的データ '{nameof(T)}' のファイル '{file.FullName}' は存在しません。");
+            }
+
+            var a = JsonConvert.DeserializeObject<JsonKeyRecord<T>>(File.ReadAllText(file.FullName), converters)
+                ?? throw new InvalidCastException();
+
+            return new KeyRecordCollection<T>(a.Items);
+        }
+
+        private class JsonKeyRecord<T> where T : KeyRecord
+        {
+            public IEnumerable<T> Items { get; set; } = Array.Empty<T>();
+        }
+    }
+}
---
+++ PokeWorld/Repositories/IStaticRepository.cs
@@ -0,0 +1,16 @@
+﻿using PokeWorld.Common;
+using PokeWorld.Models;
+
+namespace PokeWorld.Repositories
+{
+    /// <summary>
+    /// 静的データへの物理的な操作を隠蔽し、抽象度の高い操作を提供します。
+    /// </summary>
+    public interface IStaticRepository
+    {
+        /// <summary>
+        /// 種族情報の静的データ コレクションを取得します。
+        /// </summary>
+        KeyRecordCollection<Species> Species { get; }
+    }
+}
